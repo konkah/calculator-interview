88631c67fdf0fde1537c72e2eafd796cced67f64
Karlos H. Braga <karlos.hbraga@gmail.com>
2024-11-18 12:52:13 +0000

    Include bool system loop to the project

--- CI_2_IfElse_TryParse/Program.cs
+++ CI_2_IfElse_TryParse/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            while (true)
+            bool system_loop = true;
+            while (system_loop)
             {
                 Console.ForegroundColor = ConsoleColor.Yellow;
                 Console.WriteLine("Calculator:\n");
