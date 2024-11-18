b3c43296075f81b9fde33e60007857a15ae89dd5
Karlos H. Braga <karlos.hbraga@gmail.com>
2024-11-18 12:51:37 +0000

    Refactor Project switch case try parse

--- CI_1_SwitchCase_TryParse/Program.cs
+++ CI_1_SwitchCase_TryParse/Program.cs
@@ -17,73 +17,132 @@
                     "6 - Exit\n"
                  );
 
-                float num_op = float.Parse(Console.ReadLine());
+                float num_op;
+                float.TryParse(Console.ReadLine(), out num_op);
                 Console.WriteLine();
 
                 switch (num_op)
                 {
                     case 1:
-                        float result_sum = 0;
-
                         Console.WriteLine("Put the first number of the addition below:");
-                        float num1_sum = float.Parse(Console.ReadLine());
-                        Console.WriteLine("Put the second number of the addition below:");
-                        float num2_sum = float.Parse(Console.ReadLine());
-                        Console.WriteLine();
+                        float num1_sum;
+                        if (float.TryParse(Console.ReadLine(), out num1_sum))
+                        {
+                            Console.WriteLine("Put the second number of the addition below:");
+                            float num2_sum;
+                            if (float.TryParse(Console.ReadLine(), out num2_sum))
+                            {
+                                Console.WriteLine();
+                                float result_sum;
+                                result_sum = num1_sum + num2_sum;
 
-                        result_sum = num1_sum + num2_sum;
-
-                        Console.WriteLine($"Addition's result:\n{result_sum}\n\n");
+                                Console.WriteLine($"Addition's result:\n{result_sum}\n\n");
+                            }
+                            else
+                            {
+                                Console.WriteLine();
+                                Console.WriteLine("Texts are not accepted in addition operation.\n\n");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine("Texts are not accepted in addition operation.\n\n");
+                        }
 
                         break;
 
                     case 2:
-                        float result_sub = 0;
-
                         Console.WriteLine("Put the first number of the subtraction below:");
-                        float num1_sub = float.Parse(Console.ReadLine());
-                        Console.WriteLine("Put the second number of the subtraction below:");
-                        float num2_sub = float.Parse(Console.ReadLine());
-                        Console.WriteLine();
+                        float num1_sub;
+                        if (float.TryParse(Console.ReadLine(), out num1_sub))
+                        {
+                            Console.WriteLine("Put the second number of the subtraction below:");
+                            float num2_sub;
+                            if (float.TryParse(Console.ReadLine(), out num2_sub))
+                            {
+                                Console.WriteLine();
+                                float result_sub;
+                                result_sub = num1_sub - num2_sub;
 
-                        result_sub = num1_sub - num2_sub;
-
-                        Console.WriteLine($"Subtraction's result:\n{result_sub}\n\n");
+                                Console.WriteLine($"Subtraction's result:\n{result_sub}\n\n");
+                            }
+                            else
+                            {
+                                Console.WriteLine();
+                                Console.WriteLine("Texts are not accepted in subtraction operation.\n\n");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine("Texts are not accepted in subtraction operation.\n\n");
+                        }
 
                         break;
 
                     case 3:
-                        float result_mult = 0;
-
                         Console.WriteLine("Put the first number of the multiplication below:");
-                        float num1_mult = float.Parse(Console.ReadLine());
-                        Console.WriteLine("Put the second number of the multiplication below:");
-                        float num2_mult = float.Parse(Console.ReadLine());
-                        Console.WriteLine();
+                        float num1_mult;
+                        if (float.TryParse(Console.ReadLine(), out num1_mult))
+                        {
+                            Console.WriteLine("Put the second number of the multiplication below:");
+                            float num2_mult;
+                            if (float.TryParse(Console.ReadLine(), out num2_mult))
+                            {
+                                Console.WriteLine();
+                                float result_mult;
+                                result_mult = num1_mult * num2_mult;
 
-                        result_mult = num1_mult * num2_mult;
-
-                        Console.WriteLine($"Multiplication's result:\n{result_mult}\n\n");
+                                Console.WriteLine($"Multiplication's result:\n{result_mult}\n\n");
+                            }
+                            else
+                            {
+                                Console.WriteLine();
+                                Console.WriteLine("Texts are not accepted in multiplication operation.\n\n");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine("Texts are not accepted in multiplication operation.\n\n");
+                        }
 
                         break;
 
                     case 4:
-                        float result_div = 0;
-
                         Console.WriteLine("Put the first number of the division below:");
-                        float num1_div = float.Parse(Console.ReadLine());
-                        Console.WriteLine("Put the second number of the division below:");
-                        float num2_div = float.Parse(Console.ReadLine());
-                        Console.WriteLine();
-
-                        if (num2_div != 0)
+                        float num1_div;
+                        if (float.TryParse(Console.ReadLine(), out num1_div))
                         {
-                            result_div = num1_div / num2_div;
-                            Console.WriteLine($"Division's result:\n{result_div}\n\n");
+                            Console.WriteLine("Put the second number of the division below:");
+                            float num2_div;
+                            if (float.TryParse(Console.ReadLine(), out num2_div))
+                            {
+                                if (num2_div != 0)
+                                {
+                                    Console.WriteLine();
+                                    float result_div;
+                                    result_div = num1_div / num2_div;
+
+                                    Console.WriteLine($"Division's result:\n{result_div}\n\n");
+                                }
+                                else
+                                {
+                                    Console.WriteLine();
+                                    Console.WriteLine("It's not possible division by 0!\n\n");
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine();
+                                Console.WriteLine("Texts are not accepted in division operation.\n\n");
+                            }
                         }
                         else
                         {
-                            Console.WriteLine("It's not possible division by 0!\n\n");
+                            Console.WriteLine();
+                            Console.WriteLine("Texts are not accepted in division operation.\n\n");
                         }
 
                         break;
